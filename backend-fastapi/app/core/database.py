9c768a89e00d375bb7c39ed111e7961df86c68d1
diegogonzaleztoledano-12 <diego.gonzaleztoledano@riberadeltajo.es>
2026-03-11 11:56:29 +0100

    fix bd name

--- backend-fastapi/app/core/database.py
+++ backend-fastapi/app/core/database.py
@@ -2,5 +2,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import MONGODB_URL
 
 client = AsyncIOMotorClient(MONGODB_URL)
-db = client.casos_clinicos
+db = client.codecrusaders
 casos_collection = db.casos
